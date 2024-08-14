e049c63417d810fbcb15fad25d57f8d209556d0f
arielsantos.ac <arielsantos.ac@gmail.com>
2024-08-13 19:58:02 -0700

    feat: header component

---
+++ src/app/_components/header-app/header-app.module.ts
@@ -0,0 +1,15 @@
+
+import { NgModule, NO_ERRORS_SCHEMA } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HeaderAppComponent } from './header-app.component';
+
+@NgModule({
+  declarations: [
+    HeaderAppComponent
+  ],
+  imports: [
+    CommonModule
+  ],
+  schemas: [NO_ERRORS_SCHEMA],
+})
+export class HeaderAppModule { }
